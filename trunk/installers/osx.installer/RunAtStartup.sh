d3e840a077ca7639589987649fbf0caaee3c3ac5
micharu123@gmail.com <micharu123@gmail.com@38971562-6095-0d39-2460-02e45226a5be>
2011-10-12 21:08:54 +0000

    Update configure to check for python interpreters installed to /Library/Frameworks.  Update to pre-install script.

--- trunk/installers/osx.installer/RunAtStartup.sh
+++ trunk/installers/osx.installer/RunAtStartup.sh
@@ -7,18 +7,11 @@
 # Minimum spec is 2.7, the installer can handle the rest 
 # through requirement scripts.
 
-if [ -d /opt/local/Library/Frameworks/Python.framework/Versions/2.7 ]; then
-	touch /tmp/p27_macports
-elif [ -d /opt/local/Library/Frameworks/Python.framework/Versions/3.2 ]; then
-	touch /tmp/p32_macports
-elif [ -d /Library/Frameworks/Python.framework/Versions/2.7/ ]; then
-	touch /tmp/p27_installer
-elif [ -d /Library/Frameworks/Python.framework/Versions/2.7/ ]; then
-	touch /tmp/p32_installer
-elif [ -d /Library/Python/2.7/ ]; then
-	touch /tmp/p27_lion
-else
-	exit 0
+if  [ -d /opt/local/Library/Frameworks/Python.framework/Versions/2.7/ ]|| \ 
+    [ -d /opt/local/Library/Frameworks/Python.framework/Versions/3.2/ ]|| \
+    [ -d /Library/Frameworks/Python.framework/Versions/2.7/           ]|| \ 
+    [ -d /Library/Frameworks/Python.framework/Versions/3.2/           ]|| \
+    [ -d /Library/Python/2.7/ ]; then
+    exit 1
 fi 
-
-exit 1
+exit 0
